caf73a58c6665c400fbfffbc1e3aef23bd34923a
MaxMA2000 <maxmahklife@gmail.com>
2024-03-24 15:10:56 +0800

    feat(IAC-10): add get assets by type feature

--- controllers/assetController.js
+++ controllers/assetController.js
@@ -4,8 +4,20 @@ const axios = require('axios');
 
 router.get('/v1/asset', async (req, res) => {
   try {
-    console.log("sending request to get all assets")
+    console.log("sending request to get all assets");
     const response = await axios.get('http://localhost:8080/data/v1/asset');
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/v1/asset/byType', async (req, res) => {
+  try {
+    const type = req.query.type;
+    console.log(`sending request to get assets by type: ${type}`);
+    const response = await axios.get(`http://localhost:8080/data/v1/asset/byType?type=${type}`);
     res.json(response.data);
   } catch (error) {
     console.error(error);
