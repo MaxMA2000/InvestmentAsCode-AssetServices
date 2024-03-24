83048415476f4caecc49b9d1a1841e5ae19ae88f
MaxMA2000 <maxmahklife@gmail.com>
2024-03-24 15:34:20 +0800

    refactor(IAC-10): refactor the DAL_URL to config example for multiple env setting

--- controllers/assetController.js
+++ controllers/assetController.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const router = express.Router();
 const axios = require('axios');
+
+const environment = process.env.NODE_ENV || 'dev';
+const config = require('../config.json');
+const DAL_URL = config[environment].INVESTMENTASCODE_DAL_URL;
+
+console.log(`environemnt = ${environment}`)
+console.log(`DAL_URL = ${DAL_URL}`)
+
+
+const router = express.Router();
 
 router.get('/v1/asset', async (req, res) => {
   try {
     console.log("sending request to get all assets");
-    const response = await axios.get('http://localhost:8080/data/v1/asset');
+    const response = await axios.get(`${DAL_URL}/data/v1/asset`);
     res.json(response.data);
   } catch (error) {
     console.error(error);
@@ -17,7 +26,7 @@ router.get('/v1/asset/byType', async (req, res) => {
   try {
     const type = req.query.type;
     console.log(`sending request to get assets by type: ${type}`);
-    const response = await axios.get(`http://localhost:8080/data/v1/asset/byType?type=${type}`);
+    const response = await axios.get(`${DAL_URL}/data/v1/asset/byType?type=${type}`);
     res.json(response.data);
   } catch (error) {
     console.error(error);
