e8d398e0191db67dcfa9929d30d8b9c56cb1e8ad
MaxMA2000 <maxmahklife@gmail.com>
2024-04-20 14:15:58 +0800

    refactor(fetchAssetsService): refactor the controller, service

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const assetController = require('./controllers/assetController');
+const fetchAssetsController = require('./controllers/fetchAssetsController');
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use('/', assetController);
 
-app.get('/app-name', (req, res) => {
+app.get('/', (req, res) => {
   res.send('This is Asset Services');
 });
+
+app.use('/', fetchAssetsController);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
